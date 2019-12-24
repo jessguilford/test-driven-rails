a9ffe7fe680b0e8ea744cfe433328d93ffb71815
Jess <jess.guilford@gmail.com>
2019-12-23 16:21:38 -0800

    allow user to sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'todos#index'
 
   resources :todos, only: [:index, :new, :create]
+
+  resource :session, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
